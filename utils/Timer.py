05f99b4f1b5a70902704474ba647fc10ccc18fd5
Guillaume <guillaume.menard95@gmail.com>
2013-05-13 00:16:12 -0400

    Problem 42

--- utils/Timer.py
+++ utils/Timer.py
@@ -8,4 +8,4 @@ def timeExecution(func):
     start_time = time.clock()
     func()
     end_time = time.clock()
-    print('Execution time:', (end_time - start_time), 'seconds')
+    print('Execution time: %f seconds' % (end_time - start_time))
